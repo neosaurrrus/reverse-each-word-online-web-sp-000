07732bfc05aed1912e6999f57fd8ca1545c23932
Lukie Kang <lukiek+f@gmail.com>
2019-05-22 22:57:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ wordArr.collect do | word |
   letterArr = word.split("")
   revletterArr = letterArr.reverse
   reverseArr.push(revletterArr.join(""));
-
+  reverseArr
 end
 return reverseArr.join(" ")
 end
