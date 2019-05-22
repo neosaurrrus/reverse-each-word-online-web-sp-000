ee30af5d4b298f7623ab123630946a947a2113ff
Lukie Kang <lukiek+f@gmail.com>
2019-05-22 22:52:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(str)
 wordArr = str.split(" ")
 reverseArr = []
 wordArr.each do | word |
-  reverseArr.push(word.split("").reverse);
+  revWord = (word.split("")).reverse
+  reverseArr.push(revWord);
 end
 end
